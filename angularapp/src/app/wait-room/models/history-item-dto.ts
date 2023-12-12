2d3cb1ef18d804f6b1c80fb2f0c7346061350b0b
Jakub Walaszek <62243373+JWalasze@users.noreply.github.com>
2023-12-12 12:26:54 +0100

    Delete angularapp directory

--- angularapp/src/app/wait-room/models/history-item-dto.ts
+++
@@ -1,8 +0,0 @@
-
-export interface HistoryItemDto {
-  id: number;
-  player1Username: string;
-  player2Username: string;
-  duration: string;
-  status: string;
-}
--- angularapp/src/app/wait-room/models/history-item.ts
+++
@@ -1,8 +0,0 @@
-export interface HistoryItem {
-  id: number;
-  player1Username: string;
-  player2Username: string;
-  startTime: Date;
-  endTime: Date;
-  winnerUsername?: string;
-}
